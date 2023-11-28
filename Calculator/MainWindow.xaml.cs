f223a047f02c440bd9da66e811288f9c0553aca7
Akvile Lukauskaite <akvilelukauskaite17@gmail.com>
2023-11-28 11:44:22 +0100

    added some event handlers

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.CodeDom;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,11 +17,145 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            resultLabel.Content = "15";
+            acButton.Click += AcButton_Click1;
+            negButton.Click += NegButton_Click1;
+            percButton.Click += PercButton_Click1;
+            equalButton.Click += EqualButton_Click1;
+        }
+
+        private void EqualButton_Click1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void PercButton_Click1(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegButton_Click1(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click1(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void ZeroButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OneButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TwoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ThreeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FourButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FiveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SixButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            } else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+            }
+        }
+
+        private void EightButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FracButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MultButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DivButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PercButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NegButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
