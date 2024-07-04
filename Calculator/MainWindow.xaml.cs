4357203682c68f2109888da264f85f7a7acc79e1
Akvile Lukauskaite <akvilelukauskaite17@gmail.com>
2024-07-04 13:29:41 +0200

    added warning box for div by 0

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -169,6 +169,12 @@ namespace Calculator
 
             public static double Divide(double n1, double n2)
             {
+                if (n2 == 0)
+                {
+                    MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return 0;
+                }
+
                 return n1 / n2;
             }
         }
